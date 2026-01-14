312759fb2937a1e1d6662376cd0c8992dcea6862
volta2030 <volta2030@gmail.com>
2026-01-14 23:31:40 +0900

    build: Upgrade Kotlin from 2.1.0 to 2.3.0
    - Updated root build.gradle.kts
    - Updated ahmes/build.gradle.kts
    - Verified compilation with Kotlin 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "2.3.0"
     id("org.jetbrains.compose") version "1.7.3"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
-    kotlin("plugin.serialization") version "2.1.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0"
+    kotlin("plugin.serialization") version "2.3.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.4"
 }
 
